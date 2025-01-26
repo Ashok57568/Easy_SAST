059b5671027eabae70e3d2063de1ee8432307a06
Ashok Dhungana <110181629+Ashok57568@users.noreply.github.com>
2025-01-26 09:42:25 +0545

    Improvement in detection file

--- detect.py
+++ detect.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import os
 import re
 import math
@@ -30,6 +28,7 @@ def shannon_entropy(data, iterator):
             entropy += - p_x*math.log(p_x, 2)
     return entropy
 
+
 def analysis(path, plain):
     """
     Analyzes a single file's source code to identify security vulnerabilities such as hardcoded credentials
@@ -39,8 +38,28 @@ def analysis(path, plain):
     - path: Path to the file to analyze.
     - plain: Boolean flag indicating whether output should be formatted as plain text.
     """
-    
+
     global result_count
     global result_files
     result_files += 1
     with open(path, 'r', encoding='utf-8', errors='replace') as content_file:
+
+        # Clean source for a better detection
+        content = content_file.read()
+        content = clean_source_and_format(content)
+
+        # Hardcoded credentials (work as an exception, it's not function based)
+        credz = ['pass', 'secret', 'token', 'pwd', 'api-key']
+        for credential in credz:
+            content_pure = content.replace(' ', '')
+
+            # detect all variables
+            regex_var_detect = "\$[\w\s]+\s?=\s?[\"|'].*[\"|']|define\([\"|'].*[\"|']\)"
+            regex = re.compile(regex_var_detect , re.I)
+            matches = regex.findall(content_pure)
+            
+            # If we find a variable with a constant for a given indicator
+            for vuln_content in matches:
+                if credential in vuln_content.lower():
+                    payload = ["", "Hardcoded Credential", []]
+                    add_vuln_var(payload, plain, path, vuln_content, content, regex_var_detect)
