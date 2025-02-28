77c3b9feaf8ac7637eedb1960c7b57273cc11dcc
Ashok Dhungana <110181629+Ashok57568@users.noreply.github.com>
2025-02-28 18:58:14 +0545

    Detect Improvement

--- detect.py
+++ detect.py
@@ -78,3 +78,85 @@ def analysis(path, plain):
             if shannon_entropy(vuln_content, BASE64_CHARS) >= 4.1 or \
                 shannon_entropy(vuln_content, HEX_CHARS) >= 2.5:
                 add_vuln_var(payload, plain, path, vuln_content, content, regex_var_detect)
+                
+        
+        # Detection of RCE/SQLI/LFI/RFI/RFU/XSS/...
+        for payload in payloads:
+            regex = re.compile(payload[0] + regex_indicators)
+            matches = regex.findall(content.replace(" ", "(PLACEHOLDER"))
+
+            for vuln_content in matches:
+
+                # Handle "require something" vs "require(something)"
+                # Dirty trick to force a parenthesis before the function's argument
+                vuln_content = list(vuln_content)
+                for i in range(len(vuln_content)):
+                    vuln_content[i] = vuln_content[i].replace("(PLACEHOLDER", " ")
+                    vuln_content[i] = vuln_content[i].replace("PLACEHOLDER", "")
+
+                occurence = 0
+
+                # Security hole detected, is it protected ?
+                if not check_protection(payload[2], vuln_content):
+                    declaration_text, line = "", ""
+
+                    # Managing multiple variable in a single line/function
+                    sentence = "".join(vuln_content)
+                    regex = re.compile(regex_indicators[2:-2])
+                    for vulnerable_var in regex.findall(sentence):
+                        false_positive = False
+                        occurence += 1
+
+                        # No declaration for $_GET, $_POST ...
+                        if not check_exception(vulnerable_var[1]):
+                            # Look for the declaration of $something = xxxxx
+                            false_positive, declaration_text, line = check_declaration(
+                                content,
+                                vulnerable_var[1],
+                                path)
+
+                            # Set false positive if protection is in the variable's declaration
+                            is_protected = check_protection(payload[2], declaration_text)
+                            false_positive = is_protected if is_protected else false_positive
+
+                        # Display all the vuln
+                        line_vuln = find_line_vuln(payload, vuln_content, content)
+
+                        # Check for not $dest="constant"; $dest='cste'; $dest=XX;
+                        if "$_" not in vulnerable_var[1]:
+                            if "$" not in declaration_text.replace(vulnerable_var[1], ''):
+                                false_positive = True
+
+                        if not false_positive:
+                            result_count = result_count + 1
+                            display(path, payload, vuln_content, line_vuln, declaration_text, line, vulnerable_var[1], occurence, plain)
+
+
+def recursive(dir, progress, plain):
+    """
+    Recursively scans directories for PHP files and analyzes them for vulnerabilities.
+    
+    Parameters:
+    - dir: The directory to start scanning from.
+    - progress: The current depth of recursion, used for displaying progress.
+    - plain: Indicates whether to use plain text for progress indicators.
+    """
+    progress += 1
+    progress_indicator = '⬛'
+    if plain:
+        progress_indicator = "█"
+    try:
+        for name in os.listdir(dir):
+
+            print('\tAnalyzing : ' + progress_indicator * progress + '\r', end="\r"),
+
+            # Targetting only PHP Files
+            if os.path.isfile(os.path.join(dir, name)):
+                if ".php" in os.path.join(dir, name):
+                    analysis(dir + "/" + name, plain)
+            else:
+                recursive(dir + "/" + name, progress, plain)
+
+    except OSError as e:
+        print("Error 404 - Not Found, maybe you need more right ?" + " " * 30)
+        exit(-1)
