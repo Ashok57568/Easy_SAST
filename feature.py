22fce9593e9082cf4aa3d7dba29e59879c3c4a1a
Ashok Dhungana <110181629+Ashok57568@users.noreply.github.com>
2025-03-01 19:38:31 +0545

    Some feature were added

--- feature.py
+++ feature.py
@@ -10,3 +10,14 @@ def nth_replace(string, old, new, n):
         nth_split = [left_join.join(groups[:n]), right_join.join(groups[n:])]
         return new.join(nth_split)
     return string.replace(old, new)
+return new.join(nth_split)
+    return string.replace(old, new)
+
+
+def display(path, payload, vulnerability, line, declaration_text, declaration_line, colored, occurrence, plain):
+    # Display detected vulnerability information.
+    header = "{}Potential vulnerability found : {}{}{}".format('' if plain else '\033[1m', '' if plain else '\033[92m', payload[1], '' if plain else '\033[0m')
+
+    #Display the line and file path where the vulnerability was found.
+    line = "-->{}{}{} in {}".format('' if plain else '\033[92m', line, '' if plain else '\033[0m', path)
+
